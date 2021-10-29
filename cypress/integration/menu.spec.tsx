eb27fce8c36d1c64b313828af275d180fc022c24
Cory House <cory.house@carrier.com>
2021-10-29 15:24:03 -0500

    fix test

--- cypress/integration/menu.spec.tsx
+++ cypress/integration/menu.spec.tsx
@@ -13,4 +13,14 @@ describe("menu app", () => {
     cy.findByText("Chicken Tikka Masala");
     cy.findByText("BBQ Ribs");
   });
+
+  it.only("should support adding a menu item and displaying it on the home page", () => {
+    cy.findByText("Admin").click();
+    cy.findByLabelText("Name").type("Glazed donut");
+    cy.findAllByLabelText("Description").type("Tasty");
+    cy.findByLabelText("Price").type("1");
+    cy.findByRole("button", { name: "Save Menu Item" }).click();
+    cy.url().should("eq", "http://localhost:3000/"); // make sure the redirect worked.
+    cy.findAllByText("Glazed donut");
+  });
 });
