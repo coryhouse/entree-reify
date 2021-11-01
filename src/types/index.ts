532b52500e0851dbe5f95b27de8716b04d8c0703
Cory House <housecor@gmail.com>
2021-11-01 14:19:21 -0500

    centralized type

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,10 @@ export type NewMenuItem = {
   description: string;
   price: number | null;
 };
+
+export type MenuItem = {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+};
